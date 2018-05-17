a09dbb36b046a7dfb117944ed92289fe5c16cbf0
Garrett Kay <music_andpeace@hotmail.com>
2018-05-16 20:48:29 -0700

    Initial Commit

---
+++ Hashtag Local Alpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hashtag Local Alpha
+//
+//  Created by Garrett kay on 5/16/18.
+//  Copyright © 2018 Garrett Kay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
